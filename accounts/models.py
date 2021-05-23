820404d055dc00bd1f6e6c41c5e55d070bc3b38f
Timmyy3000 <timiogunme@gmail.com>
2021-05-23 23:58:18 +0100

    Create customer model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 
+class Customer (models.Model):
+
+    name = models.CharField(max_length = 200, null=True)
+    email = models.CharField(max_length = 200, null=True)
+    phone = models.CharField(max_length = 200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
