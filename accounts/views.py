84ffb7e7b81ecec4cfe356d0be14b2ab0f0b14f0
Timmyy3000 <timiogunme@gmail.com>
2021-05-24 16:53:37 +0100

    Complete management dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render
+from .models import *
 
 # home page
 def index(response):
+   
     return render(response, 'main/index.html')
 
 # dashborad view
 def dashboard(response):
-    return render(response, 'accounts/dashboard.html')
+
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = "Pending").count()
+
+    context = {
+        'orders' : orders,
+        'customers' : customers,
+        'total_orders':total_orders,
+        'total_customers' :total_customers,
+        'delivered' : delivered,
+        'pending' : pending
+    }
+    return render(response, 'accounts/dashboard.html', context)
 
 # products view 
 def products(response):
-    return render(response, 'main/products.html')
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(response, 'main/products.html', context)
 
 # customer view 
 def customers(response):
