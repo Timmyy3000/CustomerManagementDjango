a72bbc876d3d14a4fde9589f2aaff17662473f56
Timmyy3000 <timiogunme@gmail.com>
2021-05-24 15:46:46 +0100

    Added migrations

--- accounts/models.py
+++ accounts/models.py
@@ -36,6 +36,6 @@ class Order (models.Model):
             )
 
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-    # product =
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True, null=True)
     status=models.CharField(max_length=200, null=True, choices=STATUS)
