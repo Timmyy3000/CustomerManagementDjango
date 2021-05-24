6c95c2e1ce3da7a31636cfcb93e1b046d862643e
Timmyy3000 <timiogunme@gmail.com>
2021-05-24 17:53:59 +0100

    Added Material UI Design for styling

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name="dashboard"),
     path('products/', views.products, name="products"),
     path('customer/<int:pk>/', views.customer, name="customer"),
-    path('create_order/', views.create_order, name="creat_order")
+    path('create_order/', views.create_order, name="create_order")
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
