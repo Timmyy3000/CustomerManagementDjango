d2fb04fdf2cf2657034b7eda4082a861d3a0597e
Timmyy3000 <timiogunme@gmail.com>
2021-05-26 16:52:50 +0100

    Created authenticated user decorator

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,3 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+# restrict logged in user
+def unathenticated_user(view_func):
+    def wrapper_func(response, *args, **kwargs):
+        if response.user.is_authenticated :
+            return redirect('/dashboard')
+        else :
+            return view_func(response, *args, **kwargs)
+
+    return wrapper_func
+
