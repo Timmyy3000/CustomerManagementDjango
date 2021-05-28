92cbd9ba965bce6db4a606dd53b54dd66f5091b5
Timmyy3000 <timiogunme@gmail.com>
2021-05-28 19:46:44 +0100

    Password reset template

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,19 +21,22 @@ urlpatterns = [
     path('register/', views.register, name = "register"),
     path('profile/', views.user_profile, name = "profile"),
     path('settings/', views.account_settings, name = "settings"),
+
+
     path('reset_password/',
-     auth_views.PasswordResetView.as_view(),
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
      name="reset_password"),
 
     path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(), 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(), 
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
      name="password_reset_confirm"),
 
     path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(), 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
         name="password_reset_complete"),
+
 ]
