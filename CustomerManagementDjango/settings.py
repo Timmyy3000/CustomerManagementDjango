fd5e5f02f4ff763c36de6605b1429818f03be2b5
Timmyy3000 <timiogunme@gmail.com>
2021-05-29 09:59:52 +0100

    Linked postgres database

--- CustomerManagementDjango/settings.py
+++ CustomerManagementDjango/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'CustomerManagementDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Demo_test',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Timmy~zee3000',
+        'HOST' : 'localhost',
+        'PORT' : 5432
+
     }
 }
 
